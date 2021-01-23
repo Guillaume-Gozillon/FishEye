0b9a591f242e046ff92bfa7684aee2e82302628d
Guillaume <guillaumegozillon@gmail.com>
2021-01-23 21:13:32 +0100

    JSON to HTML Init

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function loadImages() {
+async function loadImages () {
   const response = await fetch('./data_photographers.json')
   const data = await response.json()
   creatCard(data.photographers)
@@ -7,9 +7,26 @@ async function loadImages() {
 loadImages()
 
 function creatCard (image) {
-  document.getElementById('test-card-photographer').innerHTML = `
-  <h3>${image.map(function(food) {
-    return food.city
-  })}</h3>
+  document.getElementById('main-index').innerHTML = `
+  <div class="container">${image.map(function(thingsIntoTheData) {
+    return ` 
+    <h1 class="principal">Nos photographes</h1>
+    <div class="photographers">
+    <img src="/ID_pict/${thingsIntoTheData.portrait}" alt="" />
+    <div class="name">${thingsIntoTheData.name}</div>
+    <div class="location">${thingsIntoTheData.city}</div>
+    <div class="bio">${thingsIntoTheData.tagline}</div>
+    <div class="price">${thingsIntoTheData.price}/jour</div>
+    <div class="hastag">
+      <div class="photographer-filter">
+        <p>#Sport</p>
+      </div>
+      <div class="photographer-filter">
+        <p>#Artchitecture</p>
+      </div>
+    </div>
+  </div>
+    `
+  }).join('')}
   `
 }
--- test.js
+++ test.js
@@ -1,18 +1,23 @@
-async function start () {
-    const response = await fetch('./data_photographers.json')
-    const data = await response.json()
-    console.log(data)
-    creatPhotographeCard(data.photographers[0])
-  }
-  
-  start()
-  
-  const writeTheCode = document.getElementById('test-card-photographer')
-  
-  function creatPhotographeCard (PhotographeCard) {
-    writeTheCode.innerHTML = `
-    ${Object.keys(PhotographeCard).map(function (breed) {
-      return breed
-    }).join(' ')}
-    `
-  }
+async function loadImages() {
+  const response = await fetch('./data_photographers.json')
+  const data = await response.json()
+  creatCard(data.photographers)
+}
+
+loadImages()
+
+function creatCard (image) {
+  document.getElementById('test-card-photographer').innerHTML = `
+  <h3 class="name">${image.map(function(food) {
+    return food.city
+  }).join(' - ')}</h3>
+  `
+}
+
+function creatCard (image) {
+  document.getElementById('test-card-photographer').innerHTML = `
+  <h3 class="name">${image.map(function(thingsIntoTheData) {
+    return thingsIntoTheData.city
+  }).join(' - ')}</h3>
+  `
+}
