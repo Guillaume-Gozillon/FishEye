d5ef88ed534cec2e99b8ffccc914853ebc6cc3e9
Guillaume <guillaumegozillon@gmail.com>
2021-02-25 18:07:28 +0100

    Cleaning

--- JS/Allclass.js
+++ JS/Allclass.js
@@ -5,40 +5,87 @@ async function loadContent () {
 }
 
 loadContent().then((data) => {
-  // Construction du DOM en instanciant la classe contenant le template literal
+  /**
+       * Recherche l'ID associé à la page
+       * @param {URLSearchParams} (window.location.search) fetch => ?id={number}
+       * @param {URLSearchParams.get()} params.get('id') => isole l'ID de l'URL
+       * @param {filter()} compare si ID = URL
+       */
 
-  const dataSortedById = new SortById(data)
-  console.log('Instance SortById :', dataSortedById)
+  const requestId = window.location.search
+  const urlParams = new URLSearchParams(requestId)
+  const id = urlParams.get('id')
+  console.log('Id :', id)
 
-  // VIGNETTE SUPERIEUR ---------------------------------------
+  // Créé un nouveau tableau filtrant le JSON si data.photographer OU data.media en fonction de l'ID
+  const photographerSorted = data.photographers
+  const resultPhotographe = photographerSorted.filter(newPhotographerArray => newPhotographerArray.id == (urlParams.get('id')))
 
-  const buildHTML = new HeaderPhotographer(dataSortedById.resultPhotographe[0].name, dataSortedById.resultPhotographe[0].id, dataSortedById.resultPhotographe[0].city, dataSortedById.resultPhotographe[0].country, dataSortedById.resultPhotographe[0].tags, dataSortedById.resultPhotographe[0].tagline, dataSortedById.resultPhotographe[0].price, dataSortedById.resultPhotographe[0].portrait)
+  const toSort = data.media
+  const mediaSorted = toSort.filter(newMediaArray => newMediaArray.photographerId == (urlParams.get('id')))
+  console.log('Tableau des photos filtrées :', mediaSorted)
+
+  // VIGNETTE SUPERIEUR ------->
+
+  const sameConst = resultPhotographe[0]
+
+  const buildHTML = new HeaderPhotographer(
+    sameConst.name,
+    sameConst.id,
+    sameConst.city,
+    sameConst.country,
+    sameConst.tags,
+    sameConst.tagline,
+    sameConst.price,
+    sameConst.portrait
+  )
 
   buildHTML.createCard()
 
   console.log('Instance buildHTML :', buildHTML)
 
-  // ITERATION DES CARTES PHOTOS ---------------------------------------
+  // ------->
+
+  // ITERATION DES CARTES PHOTOS ------->
 
   const images = []
-  for (key in dataSortedById.mediaSorted) {
+  for (key in mediaSorted) {
+    const shorterImage = mediaSorted[key]
     const imageMedia = new ImageMedia(
-      dataSortedById.mediaSorted[key].name,
-      dataSortedById.mediaSorted[key].id,
-      dataSortedById.mediaSorted[key].photographerId,
-      dataSortedById.mediaSorted[key].image,
-      dataSortedById.mediaSorted[key].tags,
-      dataSortedById.mediaSorted[key].likes,
-      dataSortedById.mediaSorted[key].date,
-      dataSortedById.mediaSorted[key].price
-      )
+      shorterImage.name,
+      shorterImage.id,
+      shorterImage.photographerId,
+      shorterImage.image,
+      shorterImage.tags,
+      shorterImage.likes,
+      shorterImage.date,
+      shorterImage.price
+    )
     images.push(imageMedia)
     imageMedia.createHTML()
   }
+
+  const videos = []
+  for (el in mediaSorted) {
+    const shorterVideo = mediaSorted[el]
+    const videoMedia = new VideoMedia(
+      shorterVideo.name,
+      shorterVideo.id,
+      shorterVideo.photographerId,
+      shorterVideo.video,
+      shorterVideo.tags,
+      shorterVideo.likes,
+      shorterVideo.date,
+      shorterVideo.price
+    )
+    videos.push(videoMedia)
+    videoMedia.createHTML()
+  }
 })
 
-
-
+/*
+const images = dataSortedById.mediaSorted.map(imageRaw => return new ImageMedia(imageRaw.name, imageRaw.id, imageRaw.photographerId, imageRaw.image, imageRaw .tags, imageRaw.likes, imageRaw.date, imageRaw.price))
+*/
 
 /*
 sortPhoto.addEventListener('change', () => {
