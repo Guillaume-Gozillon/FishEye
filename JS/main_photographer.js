5420c28318ea9d6cf5a74dd518611545d01c2c1f
Guillaume <guillaumegozillon@gmail.com>
2021-03-26 02:11:40 +0100

    Clean

--- JS/main_photographer.js
+++ JS/main_photographer.js
@@ -22,33 +22,47 @@ const urlParams = new URLSearchParams(requestId)
 loadContent().then((data) => {
 /**
  * New Array sorted with ID
- * @param {String} resultPhoto New Arr for data.photographer
- * @param {String} resultMedia New Arr for data.media
+ * @param {String} resultPhoto New array for data.photographer
+ * @param {String} resultMedia New array for data.media
  */
 
   const photo = data.photographers
-  const resultPhoto = photo.filter(photoArr => photoArr.id == (urlParams.get('id')))
+  const resultPhoto = photo
+    .filter(photoArr => photoArr.id == (urlParams.get('id')))
 
   const media = data.media
-  const resultMedia = media.filter(mediaArr => mediaArr.photographerId == (urlParams.get('id')))
+  const resultMedia = media
+    .filter(mediaArr => mediaArr.photographerId == (urlParams.get('id')))
 
-  // Const used to sort element's array
-  const sortByLike = (media.filter(mediaArr => mediaArr.photographerId == (urlParams.get('id')))).sort((a, b) => b.likes - a.likes)
+  /**
+   * Const used to sort element's array
+   * @param {String} sortByLike New array to sort by like
+   * @param {String} sortByDate New array to sort by date
+   * @param {String} sortByTitle New array to sort by title
+   */
 
-  const sortByDate = (media.filter(mediaArr => mediaArr.photographerId == (urlParams.get('id')))).sort(function (a, b) {
-    return new Date(b.date) - new Date(a.date)
-  })
+  const sortByLike = (media
+    .filter(mediaArr => mediaArr.photographerId == (urlParams.get('id'))))
+    .sort((a, b) => b.likes - a.likes)
 
-  const sortByTitle = (media.filter(mediaArr => mediaArr.photographerId == (urlParams.get('id')))).sort(function (a, b) {
-    if (a.name < b.name) { return -1 }
-    if (a.name > b.name) { return 1 }
-    return 0
-  })
+  const sortByDate = (media
+    .filter(mediaArr => mediaArr.photographerId == (urlParams.get('id'))))
+    .sort(function (a, b) {
+      return new Date(b.date) - new Date(a.date)
+    })
 
-  // Build profil photographer
+  const sortByTitle = (media
+    .filter(mediaArr => mediaArr.photographerId == (urlParams.get('id'))))
+    .sort(function (a, b) {
+      if (a.name < b.name) { return -1 }
+      if (a.name > b.name) { return 1 }
+      return 0
+    })
+
+  // Build DOM for profil photographer
   new HeaderPhotographer(resultPhoto[0])
 
-  // Build DOM photographer
+  // Build DOM for photographer image/video
   new BuildCardDOM(resultMedia, sortByLike, sortByDate, sortByTitle)
 })
 
